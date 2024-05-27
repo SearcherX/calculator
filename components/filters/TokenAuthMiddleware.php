9fa5fa6fb19ca2cf6364a30c8f4106aac9a05986
Найденов Александр <alexander.naidenov@mail.ru>
2024-05-27 09:55:33 +0300

    реализован rest api

--- components/filters/TokenAuthMiddleware.php
+++ components/filters/TokenAuthMiddleware.php
@@ -2,21 +2,26 @@
 
 namespace app\components\filters;
 
+use Dotenv\Dotenv;
 use Yii;
 use yii\base\ActionFilter;
 use yii\web\UnauthorizedHttpException;
+
+global $projectRoot;
+$projectRoot = Yii::getAlias('@app');
 
 class TokenAuthMiddleware extends ActionFilter
 {
     public function beforeAction($action): bool
     {
+        global $projectRoot;
         $token = Yii::$app->request->headers->get('X-Api-Key');
 
-        if ($token !== $_ENV['X-API-KEY']) {
-//            var_dump($token);
-            var_dump($_ENV['X-API-KEY']);
-            die();
-//            throw new UnauthorizedHttpException('Авторизация не выполнена');
+        $dotenv = Dotenv::createMutable($projectRoot);
+        $dotenv->load();
+
+        if ($token !== getenv('X_API_KEY')) {
+            throw new UnauthorizedHttpException('Авторизация не выполнена');
         }
 
         return true;
